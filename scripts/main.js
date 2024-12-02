e4d8ade6d6d064ad226440c33bfae992667d53ec
25JT <jhonierlucumimeza@gmial.com>
2024-12-02 16:01:28 -0500

    final nav texto e imagenes

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,4 @@ document.getElementById("nombtienda").innerHTML = "nombre user"
 document.getElementById("catalogo").innerHTML = "catalogo"
 document.getElementById("contactanos").innerHTML = "contactanos"
 document.getElementById("buscar").innerHTML = "buscar"
+
